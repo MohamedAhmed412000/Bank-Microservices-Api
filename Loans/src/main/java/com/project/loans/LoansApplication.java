05aadc5c4adcca73854f00bb0ba751e3368a133f
Mohamed Ahmed <memoahmed412000@gmail.com>
2025-03-14 17:21:52 +0200

    creates loans microservice

---
+++ Loans/src/main/java/com/project/loans/LoansApplication.java
@@ -0,0 +1,40 @@
+package com.project.loans;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Loans Microservice API Documentation",
+        version = "1.0",
+        description = "A simple Rest API for managing loans.",
+        contact = @Contact(
+            name = "API Support",
+            url = "https://example.com/support",
+            email = "support@example.com"
+        ),
+        license = @License(
+            name = "Apache 2.0",
+            url = "https://www.apache.org/licenses/LICENSE-2.0.html"
+        )
+    ),
+    externalDocs = @ExternalDocumentation(
+        description = "A simple REST API for managing loans",
+        url = "https://github.com/example/loans-api"
+    )
+)
+public class LoansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoansApplication.class, args);
+    }
+
+}
