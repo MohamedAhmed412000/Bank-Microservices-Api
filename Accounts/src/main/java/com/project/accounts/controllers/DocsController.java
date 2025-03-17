9205c197b5e2813692c38e7fab923f76f56df27b
Mohamed Ahmed <memoahmed412000@gmail.com>
2025-03-17 09:25:55 +0200

    Add changes for configurations

--- Accounts/src/main/java/com/project/accounts/controllers/DocsController.java
+++ Accounts/src/main/java/com/project/accounts/controllers/DocsController.java
@@ -1,6 +1,9 @@
 package com.project.accounts.controllers;
 
 import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
@@ -8,6 +11,14 @@ import org.springframework.web.servlet.view.RedirectView;
 @Hidden
 @RestController
 public class DocsController {
+
+    @Value("${build.version}")
+    private String buildVersion;
+
+    @GetMapping(value = "/api/build-version", produces = {MediaType.TEXT_PLAIN_VALUE})
+    public ResponseEntity<String> getBuildVersion() {
+        return ResponseEntity.ok(buildVersion);
+    }
 
     @GetMapping("/")
     public RedirectView redirectToSwagger() {
